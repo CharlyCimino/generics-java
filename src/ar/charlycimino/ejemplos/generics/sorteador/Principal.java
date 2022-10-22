8a998276c2e6b39710ae4f254da00cd525aab42e
Charly Cimino <carlosecimino@gmail.com>
2022-10-22 20:32:26 -0300

    Equipos Copa Libertadores 2022

--- src/ar/charlycimino/ejemplos/generics/sorteador/Principal.java
+++ src/ar/charlycimino/ejemplos/generics/sorteador/Principal.java
@@ -9,17 +9,36 @@ package ar.charlycimino.ejemplos.generics.sorteador;
 public class Principal {
 
     public static void main(String[] args) {
-        Sorteador<String> sorteo = new Sorteador<>();
-        
-        sorteo.add("UNO");
-        sorteo.add("DOS");
-        sorteo.add("TRES");
-        
+        Sorteador<Club> sorteo = new Sorteador<Club>();
+
+        System.out.println("Sorteador recién creado");
+        retardo(1);
+        System.out.println("¿Sorteador vacío? " + sorteo.isEmpty());
+        retardo(1);
+        System.out.println("Comienza la inserción de elementos");
+        retardo(1);
+        sorteo.add(new Club("Boca Juniors", PaisSudamericano.ARGENTINA));
+        sorteo.add(new Club("River Plate", PaisSudamericano.ARGENTINA));
+        sorteo.add(new Club("Flamengo", PaisSudamericano.BRASIL));
+        sorteo.add(new Club("Palmeiras", PaisSudamericano.BRASIL));
+        sorteo.add(new Club("Atlético Mineiro", PaisSudamericano.BRASIL));
+        sorteo.add(new Club("Athlético Paranaense", PaisSudamericano.BRASIL));
+        sorteo.add(new Club("Nacional", PaisSudamericano.URUGUAY));
+        sorteo.add(new Club("Peñarol", PaisSudamericano.URUGUAY));
+        retardo(1);
+        System.out.println("Fin de la inserción de elementos");
+        retardo(1);
+        System.out.println("¿Está vacío? " + sorteo.isEmpty());
+        retardo(1);
+        System.out.println("\n*** Comienza el sorteo... ***");
+        retardo(1);
         while (!sorteo.isEmpty()) {
-            System.out.println("El próximo sorteado es: " + sorteo.remove());
-        }        
-        
-        System.out.println(sorteo.remove());
+            System.out.println(sorteo.remove());
+            retardo(1);
+        }
+        System.out.println("*** Fin del sorteo ***\n");
+        retardo(1);
+        System.out.println("¿Sorteador vacío? " + sorteo.isEmpty());
     }
 
     public static void retardo(int s) {
